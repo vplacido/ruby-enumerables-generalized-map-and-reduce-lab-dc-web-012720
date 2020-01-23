8fc83a2dee8623abeb0f3d66d80106e633ba997d
Valentin Placido <valentinplacido25@gmail.com>
2020-01-23 22:59:58 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,22 @@
 # Your Code Here
+def map(source)
+  newArr = []
+  source.each do |item|
+    newArr << yield(item)
+  end 
+  newArr
+end
+
+def reduce(source, startingPoint=nil)
+  if startingPoint
+    total = startingPoint
+  else 
+    total = source[0]
+    source.shift()
+  end 
+  source.each do |item|
+    total = yield(total, item)
+  end
+  total
+end
+  
